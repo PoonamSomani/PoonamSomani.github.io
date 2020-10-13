451545da5277c2dd514b88ba41f201323a754eb6
psomani <somanipoonam7393@gmail.com>
2020-10-13 12:31:10 +0530

    Updates

---
+++ precache-manifest.296a9c77ae717077926334f21ecab281.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0b41f02080b448b3f376ff7d34c7934",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ba7838d79639f7c7df2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b37b3304950df73cca69",
+    "url": "/static/js/2.6e959c65.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/static/js/2.6e959c65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ba7838d79639f7c7df2",
+    "url": "/static/js/main.b329917e.chunk.js"
+  },
+  {
+    "revision": "32a1056aec171c3451e9",
+    "url": "/static/js/runtime-main.6da18db7.js"
+  }
+]);
